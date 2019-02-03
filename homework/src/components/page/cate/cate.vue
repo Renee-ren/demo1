1a94cb8b97f9b69e599fc9ec88e8baef7afc9990
renmengjiao <ren_mengjiao@163.com>
2019-02-03 15:43:27 +0800

    Create cate.vue

---
+++ homework/src/components/page/cate/cate.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-card>
+        <mybread firstnav="商品管理" lastnav="商品分类" />
+        <el-button type="success" plain>添加分类</el-button>
+        <el-table :data="model.menus" border max-height="250">
+            <el-table-tree-column file-icon="icon icon-file" folder-icon="icon icon-folder" prop="label" label="labelname" width="220"></el-table-tree-column>
+            <el-table-column prop="description" label="description"   width="180"></el-table-column>
+        </el-table>
+    </el-card>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            model: {
+                menus:''
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.el-button {
+    margin: 15px 0;
+}
+</style>
